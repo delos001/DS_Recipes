5dfc0983d937b8dae3ae62ef2b22fb7be83dd28a
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-20 11:50:50 -0400

    Update QDAExample.R

--- LinearDiscriminantAnalysis/QDAExample.R
+++ LinearDiscriminantAnalysis/QDAExample.R
@@ -1,5 +1,5 @@
-# This code is from StatsLearning Lect10 R A LDA 111213 (see resource in 1Note)
 
+# This code is from StatsLearning Lect10 R A LDA 111213 (see resource in 1Note)
 
 library(MASS)
 attach(Smarket)
@@ -12,3 +12,27 @@ qda.class=predict(qda.fit, Smarket.2005)$class
 addmargins(table(qda.class, Direction.2005))
 
 mean(qda.class==Direction.2005)
+
+
+#-------------------------------------------------------------------
+# EXAMPLE 2
+# ex from Q10 of Ch4
+
+library(MASS)
+
+train = (Year < 2009)
+Weekly.0910 = Weekly[!train, ]
+
+qda.fit = qda(Direction ~ Lag2, data = Weekly, subset = train)
+qda.class = predict(qda.fit, Weekly.0910)$class
+table(qda.class, Direction.0910)
+
+mean(qda.class == Direction.0910)
+
+
+qda.fit = qda(Direction ~ Lag2 + sqrt(abs(Lag2)), data = Weekly, subset = train)
+qda.class = predict(qda.fit, Weekly.0910)$class
+table(qda.class, Direction.0910)
+mean(qda.class == Direction.0910)
+
+
