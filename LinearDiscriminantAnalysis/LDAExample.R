81fc061e688e1379ac6e1fc63e58c7d6d23d25cb
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-20 11:51:46 -0400

    Update LDAExample.R

--- LinearDiscriminantAnalysis/LDAExample.R
+++ LinearDiscriminantAnalysis/LDAExample.R
@@ -30,3 +30,29 @@ lda.pred$posterior[1:20,1]
 lda.class[1:20]
 
 sum(lda.pred$posterior[,1]>0.9)
+
+
+
+
+
+#---------------------------------------------------------
+# EXAMPLE 2
+
+# example from Q10 of Ch4
+
+library(MASS)
+
+train = (Year < 2009)
+Weekly.0910 = Weekly[!train, ]
+
+lda.fit = lda(Direction ~ Lag2, data = Weekly, subset = train)
+lda.pred = predict(lda.fit, Weekly.0910)
+table(lda.pred$class, Direction.0910)
+
+mean(lda.pred$class == Direction.0910)
+
+lda.fit = lda(Direction ~ Lag2:Lag1, data = Weekly, subset = train)
+lda.pred = predict(lda.fit, Weekly.0910)
+mean(lda.pred$class == Direction.0910)
+
+
