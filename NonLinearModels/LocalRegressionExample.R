5ea14e23c8dd19ba019f00eaf1cbd03430748d66
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-22 11:44:13 -0400

    Update LocalRegressionExample.R

--- NonLinearModels/LocalRegressionExample.R
+++ NonLinearModels/LocalRegressionExample.R
@@ -3,10 +3,14 @@
 
 agelims=range(age)
 agelims
-age.grid=seq(from=agelims[1], to=agelims[2])
+age.grid=seq(from=agelims[1], 
+             to=agelims[2])
 
 
-plot(age, wage, xlim=agelims, cex=0.5, col="darkgrey")
+plot(age, wage, 
+     xlim=agelims, 
+     cex=0.5, 
+     col="darkgrey")
 title("Local Regression")
 
 # loess creates a fit at span of 0.2 and 0.6
@@ -14,10 +18,20 @@ fit = loess(wage~age, span=0.2, data = Wage)
 fit2 = loess(wage~age, span=0.6, data = Wage)
 
 # plot: using age.grid and predicted fit values from age.grid for first fit above
-lines(age.grid, predict(fit, data.frame(age=age.grid)), col="red", lwd=2)
-lines(age.grid, predict(fit2, data.frame(age=age.grid)), col="blue", lwd=2)
-legend("topright", legend=c("Span=0.2", "Span=0.5"), col=c("red", "blue"),
-       lty=1, lwd=2, cex=0.8)
+lines(age.grid, 
+      predict(fit, data.frame(age=age.grid)), 
+      col="red", 
+      lwd=2)
+lines(age.grid, 
+      predict(fit2, data.frame(age=age.grid)), 
+      col="blue", 
+      lwd=2)
+legend("topright", 
+       legend=c("Span=0.2", "Span=0.5"), 
+       col=c("red", "blue"),
+       lty=1, 
+       wd=2, 
+       cex=0.8)
 
 # gets predicted value of when age (the x value) is 80.  
 #   (this is for a local regression model)
