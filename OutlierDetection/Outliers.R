dcda7655c4539ab9652c5b17ba2784c8651af97d
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-23 12:51:26 -0400

    Update Outliers.R

--- OutlierDetection/Outliers.R
+++ OutlierDetection/Outliers.R
@@ -11,12 +11,12 @@ FARmax<-max(mydata$RATIOc[mydata$SEX=='F'])
 IARmax<-max(mydata$RATIOc[mydata$SEX=='I'])
 MARmax<-max(mydata$RATIOc[mydata$SEX=='M'])
 
-FAReol<-max(mydata$RATIOc[mydata$SEX=='F']) >= 
-          quantile(mydata$RATIOc[mydata$SEX=='F'],
-          0.75) + 3.0*IQR(mydata$RATIOc[mydata$SEX=='F'])
-IAReol<-max(mydata$RATIOc[mydata$SEX=='I']) >= 
-          quantile(mydata$RATIOc[mydata$SEX=='I'],
-          0.75) + 3.0*IQR(mydata$RATIOc[mydata$SEX=='I'])
-MAReol<-max(mydata$RATIOc[mydata$SEX=='I']) >= 
-          quantile(mydata$RATIOc[mydata$SEX=='M'],
-          0.75) + 3.0*IQR(mydata$RATIOc[mydata$SEX=='M'])
+FAReol <- max(mydata$RATIOc[mydata$SEX=='F']) >= 
+                    quantile(mydata$RATIOc[mydata$SEX=='F'],
+                             0.75) + 3.0*IQR(mydata$RATIOc[mydata$SEX=='F'])
+IAReol <- max(mydata$RATIOc[mydata$SEX=='I']) >= 
+                    quantile(mydata$RATIOc[mydata$SEX=='I'],
+                             0.75) + 3.0*IQR(mydata$RATIOc[mydata$SEX=='I'])
+MAReol <- max(mydata$RATIOc[mydata$SEX=='I']) >= 
+                    quantile(mydata$RATIOc[mydata$SEX=='M'],
+                             0.75) + 3.0*IQR(mydata$RATIOc[mydata$SEX=='M'])
