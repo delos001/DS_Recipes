68a19f4f2071aceee7854370c6424029f4c8c2bb
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-23 12:49:43 -0400

    Create Outliers.R

---
+++ OutlierDetection/Outliers.R
@@ -0,0 +1,22 @@
+
+
+
+#----------------------------------------------------------
+# Outlier detection from boxplot
+# this gets max value and sets as an object for each variable.  
+#   in this example, female abalone ration, infant abalone ratio, 
+#   male abalone ratio
+
+FARmax<-max(mydata$RATIOc[mydata$SEX=='F'])
+IARmax<-max(mydata$RATIOc[mydata$SEX=='I'])
+MARmax<-max(mydata$RATIOc[mydata$SEX=='M'])
+
+FAReol<-max(mydata$RATIOc[mydata$SEX=='F']) >= 
+          quantile(mydata$RATIOc[mydata$SEX=='F'],
+          0.75) + 3.0*IQR(mydata$RATIOc[mydata$SEX=='F'])
+IAReol<-max(mydata$RATIOc[mydata$SEX=='I']) >= 
+          quantile(mydata$RATIOc[mydata$SEX=='I'],
+          0.75) + 3.0*IQR(mydata$RATIOc[mydata$SEX=='I'])
+MAReol<-max(mydata$RATIOc[mydata$SEX=='I']) >= 
+          quantile(mydata$RATIOc[mydata$SEX=='M'],
+          0.75) + 3.0*IQR(mydata$RATIOc[mydata$SEX=='M'])
