ce7af81df0eea92a7bd736564eb4333bc7e39acc
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-23 11:31:25 -0400

    Update EDAwithR.R

--- EDA/EDAwithR.R
+++ EDA/EDAwithR.R
@@ -18,29 +18,33 @@ summary(auto$mpg)
 aggregate(Sepal.Length ~ Species, summary, data=iris)
 
 
-# SUMMARY STATISTICS CUSTOM FUNCTION
-range <- function(x) {max(x, na.rm = TRUE) - min(x, na.rm = TRUE)}  #need this line!!
-summary_stats = function(x) {
-  stats = data.frame(rbind(range(x),
-                           min(x, na.rm=TRUE),
-                           quantile(x, probs = c(0.10), na.rm = TRUE),                           
-                           quantile(x, probs = c(0.25), na.rm = TRUE),
-                           mean(x, na.rm = TRUE),
-                           median(x, na.rm = TRUE),
-                           quantile(x, probs = c(0.75), na.rm = TRUE),
-                           quantile(x, probs = c(0.90), na.rm = TRUE),
-                           max(x, na.rm=TRUE),
-                           sd(x, na.rm = TRUE),
-                           var(x, na.rm = TRUE)),
-  row.names = c("Range", "Min", "Q10", "Q25", "Mean", "Med", "Q75", 
-                "Q90", "Max", "SD", "Var"))
-  colnames(stats)=colnames(x)
-  return(stats)
+#SUMMARY STATISTICS CUSTOM FUNCTION
+range <- function(x) {max(x, na.rm = TRUE) - min(x, na.rm = TRUE)}
+cf_summ_stats_tbl = function(t){
+  cf_summ_stats = function(x) {
+    stats = data.frame(rbind(range(x),
+                             min(x, na.rm = TRUE),
+                             quantile(x, probs = c(0.10), na.rm = TRUE),                           
+                             quantile(x, probs = c(0.25), na.rm = TRUE),
+                             mean(x, na.rm = TRUE),
+                             median(x, na.rm = TRUE),
+                             quantile(x, probs = c(0.75), na.rm = TRUE),
+                             quantile(x, probs = c(0.90), na.rm = TRUE),
+                             max(x, na.rm = TRUE),
+                             sd(x, na.rm = TRUE),
+                             var(x, na.rm = TRUE)),
+                       row.names = c("Range", "Min", "Q10", "Q25", "Mean", 
+                                     "Med", "Q75", "Q90", "Max", "SD", 
+                                     "Var"))
+      colnames(stats) = colnames(x)
+    return(stats)
+  }
+  round(t(data.frame(apply(t, 2, cf_summ_stats))),2)
 }
 
-#only select variables of interst in line below
-round(t(data.frame(apply(feat_comb[, 5:24], 2, summary_stats))),2)
-round(summary_stats(feat_comb$reanalysis_air_temp_k),2)
+#-----------------------------------------------------
+# BASIC EDA WITH SMIMR
+skim(stAEp[,c(28:31, 34, 36, 37, 39, 45, 52)])
 
 #-----------------------------------------------------
 # BASIC EDA WITH fBASICS
