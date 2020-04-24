b69c879e01fd0110e8cf9745ca98961fb8295bf9
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-24 11:21:31 -0400

    Create Hypergeometric.R

---
+++ Distributions/Hypergeometric.R
@@ -0,0 +1,25 @@
+
+# Applies only to experiments with replacement 
+#   (note this is opposite of binomial distribution)
+
+# characteristics:
+# discrete distribution
+# each outcome is either success of failure
+# sampling without replacement
+# Population, N, is finite
+# number of successes in population, A, is known
+
+# see Fishers exact test for example of getting probability 
+# using hypergeometric distribution
+dhyper(x, m, n, k, log = FALSE)
+phyper(q, m, n, k, lower.tail = TRUE, log.p = FALSE)
+qhyper(p, m, n, k, lower.tail = TRUE, log.p = FALSE)
+rhyper(nn, m, n, k)
+
+
+# for:
+# x=3: if I draw 3 white balls, what is the probability given:
+# m=8 total white balls are present
+# n=16 total black balls are present (found by subtracting white balls from total balls)
+# k=5 is how many balls will be drawn from the sample
+dhyper(3,8,16,5)
