c6dff9a49a16603b0689a00805b0cb2f8aa560b4
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-24 07:57:10 -0400

    Update Normal_Gaussian.R

--- Distributions/Normal_Gaussian.R
+++ Distributions/Normal_Gaussian.R
@@ -34,6 +34,17 @@ hist(means,freq=FALSE,
      col='blue',
      main='Nile Flow Means \n1000 samples at n=100')
 
-curve(dnorm(x,mean=mm,sd=std),col = "orange", 
-      add=TRUE, lwd = 2)
-abline(v=mm,col='red',lwd=2,lty=2)
+curve(dnorm(x, mean=mm, sd=std), 
+      col = "orange", 
+      add=TRUE, 
+      lwd = 2)
+abline(v=mm,
+       col='red',
+       lwd=2,
+       lty=2)
+
+
+
+#----------------------------------------------------------
+# PNORM: 
+# pnorm: cummulative distribution function (gives probability)
