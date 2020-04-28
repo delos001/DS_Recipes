389f4587148dc3d1742ed272bd625909f4596c0e
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-28 10:11:28 -0400

    Update ConfidenceInteval.R

--- Statistical_Inference/ConfidenceInteval.R
+++ Statistical_Inference/ConfidenceInteval.R
@@ -6,7 +6,9 @@
 
 # have to get quantiles from qt using half percent quantiles 
 #   (0.005, 0.995), 24 degrees of freedom, left tail
-t.values <- qt(c(0.005, 0.995), 24, lower.tail = TRUE)
+t.values <- qt(c(0.005, 0.995), 24, 
+               lower.tail = TRUE)
 std <- sd(data.set)/sqrt(25)
 mu <- mean(data.set)
-c(signif(mu+t.values[1]*std, digits=5), signif(mu+t.values[2]*std, digits=5))
+c(signif(mu+t.values[1]*std, digits=5), 
+  signif(mu+t.values[2]*std, digits=5))
