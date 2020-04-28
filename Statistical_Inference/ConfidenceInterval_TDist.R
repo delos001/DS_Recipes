355b660b23726f60d5e44f5f335818774c4fbb62
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-28 10:08:07 -0400

    Update ConfidenceInterval_TDist.R

--- Statistical_Inference/ConfidenceInterval_TDist.R
+++ Statistical_Inference/ConfidenceInterval_TDist.R
@@ -19,7 +19,9 @@ right  # OUT: 5.936029
 # GENERAL EXAMPLE
 
 # confidence interval for before and after (difference of the means)
-t.test(post,pre,alternative=c('two.sided'),paired=TRUE, conf.level=0.95)
+t.test(post,pre,alternative=c('two.sided'),
+       paired=TRUE, 
+       conf.level=0.95)
 
 
 # gets confidence interval for t statistic (smaller sample size) 
@@ -33,5 +35,6 @@ std<-sd(post-pre)
 # df is degrees of freedom (number of samples, 
 #   use n-1 for sample and use n for population)
 # we add the error to the mean and subtract it which gives us CI
-error<-qt(0.975, df=length(pre)-1)*std/sqrt(length(pre))
-ci<-c(change-error,change+error)
+error<-qt(0.975, 
+          df = length(pre)-1) * std/sqrt(length(pre))
+ci<-c(change-error, change+error)
