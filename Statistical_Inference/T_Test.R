7df46ca641b33430e15d2bcaff81541e5c09fffc
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-28 11:56:13 -0400

    Update T_Test.R

--- Statistical_Inference/T_Test.R
+++ Statistical_Inference/T_Test.R
@@ -12,8 +12,20 @@
 # any sample size 
 # population must be normally distributed in all cases
 
-t.test(d, alternative = c("two.sided"), mu = 0, paired = FALSE, var.equal = FALSE,
+t.test(d, 
+       alternative = c("two.sided"), 
+       mu = 0, 
+       paired = FALSE,      # paried=FALSE means comparing two different groups
+       var.equal = FALSE,
        conf.level = 0.95)
 
-t.test(result$weight~result$Diet,alternative=c('two.sided'),mu=0,paired=FALSE,
-       var.equal = TRUE, conf.level=0.95)
+# result$weight~result$diet is the way to compre the weights for 
+# two groups based on diet (diet is categorized as either 1 or 3 
+# so this compares weights of diet 1 to weights of diet 2)
+
+t.test(result$weight~result$Diet,
+       alternative = c('two.sided'),
+       mu = 0,
+       paired = FALSE,
+       var.equal = TRUE, 
+       conf.level=0.95)
