171eaa2dfc33714b46bb501e6a9472a226cd62b2
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-29 14:23:37 -0400

    Update Bagging.R

--- DecisionTrees/Bagging.R
+++ DecisionTrees/Bagging.R
@@ -21,18 +21,28 @@ set.seed(1)
 # create random forest to predict medv using all data in Boston data set, 
 # using the train subset.  The mtry=13 indicates all 13 predictors should 
 # be considered for each split of the tree
-bag.boston=randomForest(medv~., data=Boston, subset = train, mtry=13, importance=TRUE)
+bag.boston=randomForest(medv~., 
+                        data=Boston, 
+                        subset = train, 
+                        mtry=13, 
+                        importance=TRUE)
 bag.boston
 
 
 # get predicted values using bagged tree results using test data [-train,]
-yhat.bag=predict(bag.boston, newdata=Boston[-train,])
+yhat.bag=predict(bag.boston, 
+                 newdata=Boston[-train,])
 plot(yhat.bag, boston.test)   # plot predicted results vs. actual results
 abline(0,1)    # generate a line with intercept at 0, slope=1
 mean((yhat.bag-boston.test)^2)   # get the MSE.  results in 13.16 
 
 # Change the number of trees grown by random forest using the ntree argument
 # same as above but the number of trees grown is 25
-bag.boston=randomForest(medv~., data=Boston, subset=train, mtry=13, ntree=25)
-yhat.bag=predict(bag.boston, newdata=Boston[-train,])
+bag.boston=randomForest(medv~., 
+                        data=Boston, 
+                        subset=train, 
+                        mtry=13, 
+                        ntree=25)
+yhat.bag=predict(bag.boston, 
+                 newdata=Boston[-train,])
 mean((yhat.bag-boston.test)^2)
