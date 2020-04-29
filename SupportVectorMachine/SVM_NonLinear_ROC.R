c70492d7781f09a966417091cc3716708441c259
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-29 12:57:51 -0400

    Create SVM_NonLinear_ROC.R

---
+++ SupportVectorMachine/SVM_NonLinear_ROC.R
@@ -0,0 +1,39 @@
+
+
+
+library(ROCR)
+rocplots=function(pred, truth, ...){
+  predob=prediction(pred, truth)
+  perf=performance(predob, "tpr", "fpr")
+  plot(perf, ...)
+}
+
+
+
+
+
+
+
+
+
+svmfit.opt=svm(y~., data=dat[train,], kernel="radial", gamma=2,
+               cost=1, decision.values=TRUE)
+fitted=attributes(predict(svmfit.opt, dat[train,], 
+                          decision.values = TRUE))$decision.values
+
+
+par(mfrow=c(1,2))
+rocplot(fitted, dat[train,"y"], main="Training Data")
+
+svmfit.flex=svm(y~., data=dat[train,], kernel ="radial",
+                    gamma=50, cost=1, decision.values=T)
+fitted=attributes(predict(svmfit.flex,dat[train ,],
+                          decision.values=T))$decision.values
+rocplot (fitted,dat[train,"y"], add =T,col ="red ")
+
+fitted=attributes(predict(svmfit.opt, dat[-train,],
+                          decision.values=TRUE))$decision.values
+rocplot(fitted, dat[-train,"y"], main="Test Data")
+fitted=attributes(predict(svmfit.flex,dat[-train,],
+                          decision.values = TRUE))$decision.values
+rocplot(fitted, dat[-train,"y"], add=TRUE, col="red")
