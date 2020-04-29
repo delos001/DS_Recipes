1d54c9dfa8f94e3801bc6ba22ae02a8b636d980a
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-29 10:44:15 -0400

    Update BayesTheorem.R

--- Stats_and_Math/Probability/BayesTheorem.R
+++ Stats_and_Math/Probability/BayesTheorem.R
@@ -6,11 +6,51 @@
 # The denominator sums up the intersection probabilities to provide the 
 #   probability of a MW hospital
 
-
+#----------------------------------------------------------
 # EXAMPLE 1
 
 hospital <- read.csv(file.path("c:/RBlack/","Hospital.csv"),sep=",")
 
-control <- factor(hospital$Control, labels = c("G_NFed","NG_NP","Profit","F_GOV"))  # create factors for Control
-region <- factor(hospital$Geog..Region, labels = c("So","NE","MW",'SW',"RM","CA","NW"))  # create factors for Geographic Region
+control <- factor(hospital$Control, 
+                  labels = c("G_NFed","NG_NP",
+                             "Profit","F_GOV"))  # create factors for Control
+region <- factor(hospital$Geog..Region, 
+                 labels = c("So","NE","MW",'SW',
+                            "RM","CA","NW"))  # create factors for Geographic Region
 control_region <- table(control, region)  # create table with both vectors
+
+
+# the numerator is the product of the marginal probability for a 
+#   for-profit hospital times the conditional probability of a MW 
+#   hospital given for-profit.  This is the intersection probability 
+#   of MW and for-profit or 11/200 (see chart above)
+numerator <- (mcr[3,8]/mcr[5,8])*(mcr[3,3]/mcr[3,8])
+
+
+# gives marginal probabilities by type of hospital:
+marginal <- mcr[,8]/mcr[5,8]
+
+
+# looks at MW hosptials  and gives marginal for MW hospitals
+conditional <- mcr[,3]/mcr[,8]
+
+
+# gives probability of various intersections between the two 
+#   (each type of hospital in the MW)
+intersection <- marginal*conditional
+
+# sum of intersection gives probability of hospital being in MW
+numerator/sum(intersection[1:4])
+
+
+#----------------------------------------------------------
+# EXAMPLE 2
+# still using hospital dataset above
+# factor service column by medical and psych
+service <- factor(hospital$Service, labels = c("medical", "psychiatric"))
+service <- addmargins(table(service))
+
+p <- service[2]/service[3]  # divide column 2 by 3 to get probability of psychiatric
+
+
+
