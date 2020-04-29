ddc31cbfdf18f724231be13de67f88d76e8baaf1
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-29 10:55:12 -0400

    Update MarginalProbability.R

--- Stats_and_Math/Probability/MarginalProbability.R
+++ Stats_and_Math/Probability/MarginalProbability.R
@@ -1,12 +1,16 @@
 
 
-control <- factor(hospital$Control, labels = c("G_NFed","NG_NP","Profit","F_GOV"))
-region <- factor(hospital$Geog..Region, labels = c("So","NE","MW",'SW',"RM","CA","NW"))
+control <- factor(hospital$Control, 
+                  labels = c("G_NFed","NG_NP",
+                             "Profit","F_GOV"))
+region <- factor(hospital$Geog..Region, 
+                 labels = c("So","NE","MW",'SW',
+                            "RM","CA","NW"))
 control_region <- table(control, region)
 
 
 # Marginal probability hospital is for profit?  
-# This is using a table and indexing the values by row and column (so mcr[row3,col8]
+# Uses a table and indexing the values by row and column (so mcr[row3,col8]
 mcr[3,8]/mcr[5,8]
 
 # Union probability Rocky Mountain or Not Government Not for-profit?
