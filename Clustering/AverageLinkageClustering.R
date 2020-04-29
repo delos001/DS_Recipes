136894c6170566f2bda99b7bbba6a0c0d838ca38
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-29 12:42:47 -0400

    Update AverageLinkageClustering.R

--- Clustering/AverageLinkageClustering.R
+++ Clustering/AverageLinkageClustering.R
@@ -1,6 +1,12 @@
 # A type of hierarchal clustering linkage
 
 plot(hclust(as.dist(illustration.data),
-  'complete'), col = 4)
+            'complete'), col = 4)
+
+
+cutree(hclust(as.dist(illustration.data),
+              'complete'), 2)
+
   
   
+## NEED TO FIND FULL EXAMPLE
