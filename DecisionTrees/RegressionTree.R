9b55d16ee800b61adcc6013c5fc08f6ede0ac6f6
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-29 14:40:44 -0400

    Update RegressionTree.R

--- DecisionTrees/RegressionTree.R
+++ DecisionTrees/RegressionTree.R
@@ -6,18 +6,27 @@ library(rpart)
 car.test.frame
 
 # sets variable containing the decision tree splits:
-x=rpart(car.test.frame[,c(1,4,6,7,8)],cp=0)  
+x = rpart(car.test.frame[,c(1,4,6,7,8)],cp=0)  
 
 
 
 predict(x)
-cor(car.test.frame[,1],predict(x))^2
+cor(car.test.frame[,1], predict(x))^2
 
-par(mai=c(0.1, 0.1, 0.1, 0.1))
+par(mai = c(0.1, 0.1, 0.1, 0.1))
 
 # this will produce a plot of a tree with branches
-plot(x, main="Regression Tree: car.test.frame", col=3, compress=TRUE, branch=0.2,
+plot(x, 
+     main="Regression Tree: car.test.frame", 
+     col=3, 
+     compress=TRUE, 
+     branch=0.2,
      uniform=TRUE)
 
 # puts the values and the numbser on lines and in boxes
-text(x, cex=0.6, col=4, use.n = TRUE, fancy = TRUE, fwidth = 0.4, fheight = 0.4, bg=c(5))
+text(x, cex=0.6, col=4, 
+     use.n = TRUE, 
+     fancy = TRUE, 
+     fwidth = 0.4, 
+     fheight = 0.4, 
+     bg=c(5))
