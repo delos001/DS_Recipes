53a1a6bff654bfbe573a479df08525f534b0283c
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-29 09:19:50 -0400

    Update BartlettsTest.R

--- Stats_and_Math/HypothesisTesting/BartlettsTest.R
+++ Stats_and_Math/HypothesisTesting/BartlettsTest.R
@@ -2,4 +2,4 @@
 # tests homegenuity across data groups
 # test homogenuity of shuck residuals across Class groups contained in the df2
 
-bartlett.test(shuckregress$residuals~Class,data=df2)
+bartlett.test(shuckregress$residuals~Class, data = df2)
