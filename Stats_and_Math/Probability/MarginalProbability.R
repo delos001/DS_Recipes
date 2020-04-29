578cd3c7d53561cf9bf29c09edc74875372f17f2
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-29 10:54:44 -0400

    Create MarginalProbability.R

---
+++ Stats_and_Math/Probability/MarginalProbability.R
@@ -0,0 +1,19 @@
+
+
+control <- factor(hospital$Control, labels = c("G_NFed","NG_NP","Profit","F_GOV"))
+region <- factor(hospital$Geog..Region, labels = c("So","NE","MW",'SW',"RM","CA","NW"))
+control_region <- table(control, region)
+
+
+# Marginal probability hospital is for profit?  
+# This is using a table and indexing the values by row and column (so mcr[row3,col8]
+mcr[3,8]/mcr[5,8]
+
+# Union probability Rocky Mountain or Not Government Not for-profit?
+(mcr[5,5]+mcr[2,8]-mcr[2,5])/mcr[5,8]
+
+# Intersection probability for-profit in California?
+mcr[3,6]/mcr[5,8]
+
+# Conditional probability hospital is in Midwest if for-profit?
+mcr[3,3]/mcr[3,8]
