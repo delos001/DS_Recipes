4afc3d449cf7bd34a24d5b4b62b376725d63517c
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-30 13:24:16 -0400

    Update ErrorTrendSeasonalSTLF.R

--- TimeSeries/ErrorTrendSeasonalSTLF.R
+++ TimeSeries/ErrorTrendSeasonalSTLF.R
@@ -1,12 +1,18 @@
 
 
 # use this if you have frequency greater than 24
-
-flu_stlf = stlf(flu_ts, h = 52, s.window = "periodic", t.window = NULL,
-                robust = FALSE, lambda = NULL, biasadj = FALSE)
+# see ErroTrendSeasonal.R for description of function params
+flu_stlf = stlf(flu_ts, 
+                h = 52, 
+                s.window = "periodic", 
+                t.window = NULL,
+                robust = FALSE, 
+                lambda = NULL, 
+                biasadj = FALSE)
 
 plot(flu_stlf)
-plot(forecast(flu_stlf, h=3), ylab="Oil (millions of tones)")
+plot(forecast(flu_stlf, h=3), 
+     ylab="Oil (millions of tones)")
 fit$par
 
 summary(flu_stlf)
