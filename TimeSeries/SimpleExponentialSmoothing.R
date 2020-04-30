f6370297230f3ebe7b11ad9e58817fe8db304c09
delos001 <37164608+delos001@users.noreply.github.com>
2020-04-30 14:26:45 -0400

    Update SimpleExponentialSmoothing.R

--- TimeSeries/SimpleExponentialSmoothing.R
+++ TimeSeries/SimpleExponentialSmoothing.R
@@ -11,16 +11,26 @@ flu_decomp$random
 
 plot(flu_decomp)
 
+# remove the seasonal component from the data
 flu_seas_adj = flu_ts - flu_decomp$seasonal
 
+#simple exponential smoothing for seasonally adjusted flu data
+# with an alpha of 0.2, 0.5, 0.8 (see text for alpha impact), 
+# initial=simple is how R initializes the forecast
 flu_seas_adj_SES = ses(flu_seas_adj, h=20)
-flu_seas_adj_SES1 = ses(flu_seas_adj, alpha=0.2, initial="simple", h=20)
-flu_seas_adj_SES2 = ses(flu_seas_adj, alpha=0.5, initial="simple", h=20)
-flu_seas_adj_SES3 = ses(flu_seas_adj, alpha=0.8, initial="simple", h=20)
+flu_seas_adj_SES1 = ses(flu_seas_adj, alpha=0.2, 
+                        initial="simple", h=20)
+flu_seas_adj_SES2 = ses(flu_seas_adj, alpha=0.5, 
+                        initial="simple", h=20)
+flu_seas_adj_SES3 = ses(flu_seas_adj, alpha=0.8, 
+                        initial="simple", h=20)
 
-plot(flu_seas_adj, type="l",
-     xlab="Weekly Data", ylab="Influenza Deaths (adjusted)",
+plot(flu_seas_adj, type="l",       # plot the original data
+     xlab="Weekly Data", 
+     ylab="Influenza Deaths (adjusted)",
      main="Seasonally Decomposed Simple Exponential Smoothing: Influenza Deaths 2009-2017")
-lines(fitted(flu_seas_adj_SES1), col="red", type="l")
+
+# plot the predicted lines over top of the plot from line above
+lines(fitted(flu_seas_adj_SES1), col="red", type="l") 
 lines(fitted(flu_seas_adj_SES2), col="blue", type="l")
 lines(fitted(flu_seas_adj_SES3), col="green", type="l")
