46dac223b07734eefab2a5e245e474a2264d8ea9
delos001 <37164608+delos001@users.noreply.github.com>
2020-05-01 09:52:24 -0400

    Update BoxTest_Example.R

--- TimeSeries/ModelEvaluation/BoxTest_Example.R
+++ TimeSeries/ModelEvaluation/BoxTest_Example.R
@@ -1,6 +1,13 @@
 
 if result exceeds the significance level of 0.05: indicates a lack of support for non-zero autocorrelations
 
-flu_seas_fit = Arima(log_flu_ts, order=c(2,1,2), seasonal=c(0,1,1))
+flu_seas_fit = Arima(log_flu_ts, 
+                     order=c(2,1,2), 
+                     seasonal=c(0,1,1))
+
 tsdisplay(residuals(flu_seas_fit))
-Box.test(residuals(flu_seas_fit), lag=52, fitdf=4, type="Ljung")
+
+Box.test(residuals(flu_seas_fit), 
+         lag=52, 
+         fitdf=4, 
+         type="Ljung")
