ff3aac578d0628b9402c4bed4d7a4ec0b61ddbe7
delos001 <37164608+delos001@users.noreply.github.com>
2020-05-01 09:50:05 -0400

    Update ResidualDiagnostics.R

--- TimeSeries/ModelEvaluation/ResidualDiagnostics.R
+++ TimeSeries/ModelEvaluation/ResidualDiagnostics.R
@@ -3,10 +3,17 @@
 # example from dow jones data  (see onenote)
 
 dj2 <- window(dj, end=250)
-plot(dj2, main="Dow Jones Index (daily ending 15 Jul 94)", 
-  ylab="", xlab="Day")
+plot(dj2, 
+     main = "Dow Jones Index (daily ending 15 Jul 94)", 
+     ylab = "", 
+     xlab = "Day")
+
 res <- residuals(naive(dj2))
-plot(res, main="Residuals from naive method", 
-  ylab="", xlab="Day")
-Acf(res, main="ACF of residuals")
-hist(res, nclass="FD", main="Histogram of residuals")
+
+plot(res, 
+     main = "Residuals from naive method", 
+     ylab = "", 
+     xlab = "Day")
+Acf(res, main = "ACF of residuals")
+hist(res, nclass="FD", 
+     main = "Histogram of residuals")
