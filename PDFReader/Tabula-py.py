db1be3973b01749f7e5866e10068aef4f20ab119
delos001 <37164608+delos001@users.noreply.github.com>
2020-05-08 15:21:04 -0400

    Create Tabula-py.py

---
+++ PDFReader/Tabula-py.py
@@ -0,0 +1,7 @@
+# A simple Python wrapper of tabula-java, which can read the table of PDF. 
+# You can read tables from PDF and convert into pandas’ DataFrame. 
+# tabula-py also enables you to convert a PDF file into CSV/TSV/JSON file.
+
+# https://towardsdatascience.com/python-for-pdf-ef0fac2808b0
+	
+# https://github.com/chezou/tabula-py
