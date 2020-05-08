fb25da96c24881726f46c85feeb24e4e7c2946db
delos001 <37164608+delos001@users.noreply.github.com>
2020-05-08 12:29:21 -0400

    Update PersitenceModelForecast.py

--- TimeSeries/PersitenceModelForecast.py
+++ TimeSeries/PersitenceModelForecast.py
@@ -24,7 +24,12 @@ from matplotlib import pyplot
 # load dataset
 def parser(x):
 	return datetime.strptime('190'+x, '%Y-%m')
-series = read_csv('shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+series = read_csv('shampoo-sales.csv', 
+		  header=0, 
+		  parse_dates=[0], 
+		  index_col=0, 
+		  squeeze=True, 
+		  date_parser=parser)
 
 # split data into train and test
 X = series.values
