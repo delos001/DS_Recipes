1763db2c82e5b87844ea50e89d79386332d9ee5c
delos001 <37164608+delos001@users.noreply.github.com>
2020-05-08 12:10:01 -0400

    Update XOR_NeuralNetwork.py

--- NeuralNetworks/XOR_NeuralNetwork.py
+++ NeuralNetworks/XOR_NeuralNetwork.py
@@ -26,11 +26,18 @@ import math
 class NeuralNetwork:
     LEARNING_RATE = 0.5
 
-    def __init__(self, num_inputs, num_hidden, num_outputs, hidden_layer_weights = None, hidden_layer_bias = None, output_layer_weights = None, output_layer_bias = None):
+    def __init__(self, num_inputs, 
+                 num_hidden, num_outputs, 
+                 hidden_layer_weights = None, 
+                 hidden_layer_bias = None, 
+                 utput_layer_weights = None, 
+                 output_layer_bias = None):
         self.num_inputs = num_inputs
 
-        self.hidden_layer = NeuronLayer(num_hidden, hidden_layer_bias)
-        self.output_layer = NeuronLayer(num_outputs, output_layer_bias)
+        self.hidden_layer = NeuronLayer(num_hidden, 
+                                        hidden_layer_bias)
+        self.output_layer = NeuronLayer(num_outputs, 
+                                        output_layer_bias)
 
         self.init_weights_from_inputs_to_hidden_layer_neurons(hidden_layer_weights)
         self.init_weights_from_hidden_layer_neurons_to_output_layer_neurons(output_layer_weights)
@@ -41,19 +48,19 @@ class NeuralNetwork:
             for i in range(self.num_inputs):
                 if not hidden_layer_weights:
                     self.hidden_layer.neurons[h].weights.append(random.random())
-                else:
-                    self.hidden_layer.neurons[h].weights.append(hidden_layer_weights[weight_num])
-                weight_num += 1
-
+                    else:
+                        self.hidden_layer.neurons[h].weights.append(hidden_layer_weights[weight_num])
+                        weight_num += 1
+                        
     def init_weights_from_hidden_layer_neurons_to_output_layer_neurons(self, output_layer_weights):
         weight_num = 0
         for o in range(len(self.output_layer.neurons)):
             for h in range(len(self.hidden_layer.neurons)):
                 if not output_layer_weights:
                     self.output_layer.neurons[o].weights.append(random.random())
-                else:
-                    self.output_layer.neurons[o].weights.append(output_layer_weights[weight_num])
-                weight_num += 1
+                    else:
+                        self.output_layer.neurons[o].weights.append(output_layer_weights[weight_num])
+                        weight_num += 1
 
     def inspect(self):
         print('------')
@@ -71,7 +78,8 @@ class NeuralNetwork:
         return self.output_layer.feed_forward(hidden_layer_outputs)
 
     # Uses online learning, ie updating the weights after each training case
-    def train(self, training_inputs, training_outputs):
+    def train(self, training_inputs, 
+              training_outputs):
         self.feed_forward(training_inputs)
 
         # 1. Output neuron deltas
@@ -121,8 +129,8 @@ class NeuralNetwork:
             self.feed_forward(training_inputs)
             for o in range(len(training_outputs)):
                 total_error += self.output_layer.neurons[o].calculate_error(training_outputs[o])
-        return total_error
-
+                return total_error
+            
 class NeuronLayer:
     def __init__(self, num_neurons, bias):
 
@@ -132,27 +140,27 @@ class NeuronLayer:
         self.neurons = []
         for i in range(num_neurons):
             self.neurons.append(Neuron(self.bias))
-
+            
     def inspect(self):
         print('Neurons:', len(self.neurons))
         for n in range(len(self.neurons)):
             print(' Neuron', n)
             for w in range(len(self.neurons[n].weights)):
                 print('  Weight:', self.neurons[n].weights[w])
-            print('  Bias:', self.bias)
-
+                print('  Bias:', self.bias)
+                
     def feed_forward(self, inputs):
         outputs = []
         for neuron in self.neurons:
             outputs.append(neuron.calculate_output(inputs))
-        return outputs
-
+            return outputs
+        
     def get_outputs(self):
         outputs = []
         for neuron in self.neurons:
             outputs.append(neuron.output)
-        return outputs
-
+            return outputs
+        
 class Neuron:
     def __init__(self, bias):
         self.bias = bias
@@ -193,29 +201,35 @@ class Neuron:
     # = 2 * 0.5 * (target output - actual output) ^ (2 - 1) * -1
     # = -(target output - actual output)
     #
-    # The Wikipedia article on backpropagation [1] simplifies to the following, but most other learning material does not [2]
+    # The Wikipedia article on backpropagation [1] simplifies to the following, 
+    # but most other learning material does not [2]
     # = actual output - target output
     #
-    # Alternative, you can use (target - output), but then need to add it during backpropagation [3]
+    # Alternative, you can use (target - output), but then need to add it during 
+    # backpropagation [3]
     #
     # Note that the actual output of the output neuron is often written as yⱼ and target output as tⱼ so:
     # = ∂E/∂yⱼ = -(tⱼ - yⱼ)
     def calculate_pd_error_wrt_output(self, target_output):
         return -(target_output - self.output)
 
-    # The total net input into the neuron is squashed using logistic function to calculate the neuron's output:
+    # The total net input into the neuron is squashed using logistic function to 
+    # calculate the neuron's output:
     # yⱼ = φ = 1 / (1 + e^(-zⱼ))
-    # Note that where ⱼ represents the output of the neurons in whatever layer we're looking at and ᵢ represents the layer below it
+    # Note that where ⱼ represents the output of the neurons in whatever layer we're 
+    # looking at and ᵢ represents the layer below it
     #
     # The derivative (not partial derivative since there is only one variable) of the output then is:
     # dyⱼ/dzⱼ = yⱼ * (1 - yⱼ)
     def calculate_pd_total_net_input_wrt_input(self):
         return self.output * (1 - self.output)
 
-    # The total net input is the weighted sum of all the inputs to the neuron and their respective weights:
+    # The total net input is the weighted sum of all the inputs to the neuron and their 
+    # respective weights:
     # = zⱼ = netⱼ = x₁w₁ + x₂w₂ ...
     #
-    # The partial derivative of the total net input with respective to a given weight (with everything else held constant) then is:
+    # The partial derivative of the total net input with respective to a given weight 
+    # (with everything else held constant) then is:
     # = ∂zⱼ/∂wᵢ = some constant + 1 * xᵢw₁^(1-0) + some constant ... = xᵢ
     def calculate_pd_total_net_input_wrt_weight(self, index):
         return self.inputs[index]
@@ -224,10 +238,18 @@ class Neuron:
 
 # Blog post example:
 
-nn = NeuralNetwork(2, 2, 2, hidden_layer_weights=[0.15, 0.2, 0.25, 0.3], hidden_layer_bias=0.35, output_layer_weights=[0.4, 0.45, 0.5, 0.55], output_layer_bias=0.6)
+nn = NeuralNetwork(2, 2, 2, 
+                   hidden_layer_weights=[0.15, 0.2, 0.25, 0.3], 
+                   hidden_layer_bias=0.35, 
+                   output_layer_weights=[0.4, 0.45, 0.5, 0.55], 
+                   output_layer_bias=0.6)
+
 for i in range(10000):
     nn.train([0.05, 0.1], [0.01, 0.99])
-    print(i, round(nn.calculate_total_error([[[0.05, 0.1], [0.01, 0.99]]]), 9))
+    print(i, 
+          round(nn.calculate_total_error([[[0.05, 0.1], 
+                                           [0.01, 0.99]]]), 
+                9))
 
 # XOR example:
 
