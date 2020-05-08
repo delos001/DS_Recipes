b44ff9fbc466f5d2948ce106b6cc96ebf60b5ec1
delos001 <37164608+delos001@users.noreply.github.com>
2020-05-08 15:19:59 -0400

    Create PyPDF2.py

---
+++ PDFReader/PyPDF2.py
@@ -0,0 +1,15 @@
+
+
+# https://github.com/mstamy2/PyPDF2
+
+# https://automatetheboringstuff.com/chapter13/
+# https://www.geeksforgeeks.org/working-with-pdf-files-in-python/
+
+
+# import nltk
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+#nltk downloader to chose packages
+nltk.download()
