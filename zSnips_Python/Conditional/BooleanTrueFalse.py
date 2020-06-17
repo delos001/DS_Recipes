ce241e0b331dec690ca533c12482c52ec6119b8c
delos001 <37164608+delos001@users.noreply.github.com>
2020-06-17 09:16:45 -0400

    Update BooleanTrueFalse.py

--- zSnips_Python/Conditional/BooleanTrueFalse.py
+++ zSnips_Python/Conditional/BooleanTrueFalse.py
@@ -4,6 +4,13 @@
 # Or statements
 # nested and/or/not statements
 
+#---------------------------------------------------------
+## Basic Boolean
+print(t and f) # Logical AND; False
+print(t or f)  # Logical OR;  True
+print(not t)   # Logical NOT; False
+print(t != f)  # Logical XOR; True
+
 
 #----------------------------------------------------------
 # NOT STATEMENT
