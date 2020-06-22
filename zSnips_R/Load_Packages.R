aedef40b599fbef0fb4bf93f0b150d5169bce38a
delos001 <37164608+delos001@users.noreply.github.com>
2020-06-22 17:29:34 -0400

    Update Load_Packages.R

--- zSnips_R/Load_Packages.R
+++ zSnips_R/Load_Packages.R
@@ -5,7 +5,7 @@
 lpkgs = c('haven', 'labelled', 'stringr', 'stringi', 'dplyr', 'lubridate')
 
 # loop through required packages & if not already installed, load, then install
-for(pkg in lshPackages){
+for(pkg in lpkgs){
   
   if (!require(pkg, character.only = TRUE)){
     install.packages(pkg)
