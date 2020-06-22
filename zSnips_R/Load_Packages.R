0e6766e91a7ce1711f79945d18ebc29289ca0aaf
delos001 <37164608+delos001@users.noreply.github.com>
2020-06-22 17:28:21 -0400

    Update function name

--- zSnips_R/Load_Packages.R
+++ zSnips_R/Load_Packages.R
@@ -2,7 +2,7 @@
 
 # Load Packages from a List
 
-lshPackages = c('haven', 'labelled', 'stringr', 'stringi', 'dplyr', 'lubridate')
+lpkgs = c('haven', 'labelled', 'stringr', 'stringi', 'dplyr', 'lubridate')
 
 # loop through required packages & if not already installed, load, then install
 for(pkg in lshPackages){
