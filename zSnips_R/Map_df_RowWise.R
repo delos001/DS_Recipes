0875efa27ea21f5ba3e92293944cef79734c623a
delos001 <37164608+delos001@users.noreply.github.com>
2020-10-20 13:40:41 -0400

    Update Map_df_RowWise.R

--- zSnips_R/Map_df_RowWise.R
+++ zSnips_R/Map_df_RowWise.R
@@ -1,9 +1,17 @@
+lpkgs = c('dplyr', 'purrr')
 
+for(pkg in lpkgs) {
+  
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg)
+    library(pkg, character.only = TRUE)
+  }
+}
 
 ## 2 df's: need each row in dfVisits to be 'assigned' to each site in dfSite
 
-dfSite = data.frame(Site = c('1001', '1002', '1003', '1004'))
-dfVisits = data.frame(Visits = c('Scr', 'BL', 'D1'))
+dfSubject = data.frame(Subject = c('1001', '1002', '1003', '1004'))
+dfVisits = data.frame(Visits = c('Scr', 'BL', 'D1', 'D2', 'W1', 'W2', 'W3'))
 
 ## so it would look like the following dataframe:
 ##  Site  Visit
@@ -15,18 +23,8 @@ dfVisits = data.frame(Visits = c('Scr', 'BL', 'D1'))
 ##  1002  D1
 ## etc...
 
-lpkgs = c('dplyr', 'purrr')
 
-for(pkg in lpkgs) {
-  
-  if (!require(pkg, character.only = TRUE)) {
-    install.packages(pkg)
-    library(pkg, character.only = TRUE)
-  }
-}
-
-## solution:
-testMap2 = purrr::map2_df(dfSite, dfVisits, function(x, y){
-  data.frame(dfSite = x, dfVisits = y )
-}) %>%
-  dplyr::arrange(dfSite)
+testMap3 = purrr::map2_df(dfSubject, t(dfVisits), function(x, y){
+  data.frame(Subject = x, Visit = y)
+  }) %>%
+  dplyr::arrange(Subject)
