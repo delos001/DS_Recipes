1ad9b40b5888ac53887f1314a2166012a465c194
Jason Delosh <ja903976@bmrn.com>
2021-01-06 11:14:38 -0500

    fuzzy join example

--- zSnips_R/Joins/Join_On_DateRange.R
+++ zSnips_R/Joins/Join_On_DateRange.R
@@ -3,12 +3,12 @@
 ##   In this snippet, 3 diff methods for date joining
 ##      1: data.table
 ##      2: sqldf
-##      3: fuzz join
+##      3: fuzzy join
 
 
 
 ##------------------------------------------------------------------------------
-## Data.table date range join
+## DATA.TABLE PACKAGE date range join
 library(data.table)
 
 X <- data.table(beginning = 
@@ -42,7 +42,7 @@ testjoin
 
 
 ##------------------------------------------------------------------------------
-## sqldf date range join
+## SQLDF PACKAGE date range join
 ##    left and right join and also a full outer join which isn't complete
 
 
@@ -64,16 +64,16 @@ B <- data.frame(aDate =
                 other = c('a', 'b', 'c', 'd', 'e'))
 
 ## join A on B where aDate is between the beginning and ending dates in A
-sqldatejoin = sqldf('select * from B
+sqldatejoin = sqldf('SELECT * FROM B
                     left outer join A
-                    on B.aDate between A.beginning and A.ending')
+                    ON B.aDate BETWEEN A.beginning AND A.ending')
 
 sqldatejoin
 
 ## same but reverse the tables.  Notice the difference in outputs
-sqldatejoin2 = sqldf('select * from A
+sqldatejoin2 = sqldf('SELECT * FROM A
                     left outer join B
-                    on B.aDate between A.beginning and A.ending')
+                    ON B.aDate BETWEEN A.beginning AND A.ending')
 
 sqldatejoin2
 
@@ -91,10 +91,65 @@ sqldf("select * from dat1 left outer join dat2 on dat1.x = dat2.w UNION
 
 ## using same sample data as above:
 ## note: this isn't final output.  Union mixes up columns  **NEED TO FIX
-sqldatejoin3 = sqldf('select * from A 
+sqldatejoin3 = sqldf('SELECT * FROM A 
                      left outer join B 
-                     on B.aDate between A.beginning and A.ending UNION 
-                     select * from B 
+                     on B.aDate BETWEEN A.beginning AND A.ending UNION 
+                     select * FROM B 
                      left outer join A 
-                     on B.aDate between A.beginning and A.ending')
+                     ON B.aDate BETWEEN A.beginning AND A.ending')
+
+
+
+
+##------------------------------------------------------------------------------
+## FUZZYJOIN date range join
+##    https://cran.r-project.org/web/packages/fuzzyjoin/fuzzyjoin.pdf
+##    there are multiple methods to join on dates with this package
+##      this example uses interval_join
+
+##  NOTE: fuzzy join has high level of dependencies which can impact performance
+##     on large data sets.  Interval_join function requires IRanges package from
+##     Bioconductor
+
+library(fuzzyjoin)
+
+## many dependencies:
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("IRanges")
+
+C <- data.frame(beginning = 
+                  as.Date(c('2010-01-15', '2010-01-24', '2010-01-15', 
+                            '2010-01-24', '2010-01-15')), 
+                ending = 
+                  as.Date(c('2010-01-22','2010-01-28','2010-01-22',
+                            '2010-01-28', '2010-01-22')),
+                name = c('001','001','002','002', '005'))
+
+D <- data.frame(aDate = 
+                  as.Date(c('2010-01-23', '2010-01-26', '2010-01-17', 
+                            '2010-01-27', '2010-01-23')),
+                name = c('001','001','002','002', '003'),
+                other = c('a', 'b', 'c', 'd', 'e'))
+
+
+## left join example
+##    options also include inner, right, full, semi, anti
+##  notice order of tables and order of range dates vs. single join date
+fzzyJoin = fuzzy_left_join(D, C,
+                            by = c('name', 
+                                   'aDate' = 'beginning', 
+                                   'aDate' = 'ending'),
+                            match_fun = list(`==`, `>=`, `<=`)
+                            )
+
+fzzyJoin
+
+lbcovance_rawtest = lbcovance_raw %>%
+  dplyr::filter(LBORRES == "NA") %>%
+  dplyr::select(LBTEST)
+
+table(lbcovance_rawtest$LBTEST)
+
+
 
